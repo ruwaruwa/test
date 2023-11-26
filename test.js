d073ad77eb087d208f52f3b4141966c2387c294e
ruwaruwa <almozali287@gmail.com>
2023-11-26 00:48:12 -0800

    test

--- test.js
+++ test.js
@@ -8,15 +8,21 @@
 const btnon=document.querySelector('#btnon')
     
 const btnof=document.querySelector('#btnof')
-const boxlight= document.querySelector('.box-light')
+// const boxlight= document.querySelector('.box-light')
+const imgOne= document.querySelector('.on')
+const imgtwo= document.querySelector('.off')
 
 btnon.addEventListener('click',function(){
-    boxlight.style.backgroundColor='yellow';
+    // boxlight.style.backgroundColor='yellow';
+    imgOne.style.display='block'
+    imgtwo.style.display='none'
 btnon.style.color='red';
 btnof.style.color='black'
 })
 btnof.addEventListener('click',function(){
-    boxlight.style.backgroundColor="transparent";
+    // boxlight.style.backgroundColor="transparent";bl
+    imgOne.style.display='none'
+    imgtwo.style.display='block'
     btnof.style.color="red"
     btnon.style.color='black'
 })
